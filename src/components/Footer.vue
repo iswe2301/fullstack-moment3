79c5382b34527797b380d60162c376a1aa4453c9
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-08 14:45:31 +0100

    Lagt till en ny komponent för footer som visar aktuellt år och styling med bootstrap.

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="bg-dark text-white text-center py-3 mt-4">
+        <div class="container">
+            <!-- Skriv ut aktuellt år -->
+            <p>&copy; {{ currentYear }} Isa Westling</p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data() {
+        return {
+            // Hämta aktuellt år
+            currentYear: new Date().getFullYear(),
+        };
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
