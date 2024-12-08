1c4e4a4a3c40152bc104b1fea2098b0647391dd9
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-08 14:45:54 +0100

    Lagt till Footer-komponenten och justerat layouten med main-tag för bättre struktur och styling.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <script>
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue'; // Importera Header-komponenten
+import Footer from './components/Footer.vue'; // Importera Footer-komponenten
+
 export default {
   name: "App",
   components: {
     Header, // Lägg till Header-komponenten
     RouterView, // Lägg till RouterView-komponenten
+    Footer, // Lägg till Footer-komponenten
   },
 };
 </script>
 
 <template>
   <Header /> <!-- Lägg till Header-komponenten -->
-  <RouterView /> <!-- Rendera vyer här -->
+  <main>
+    <RouterView /> <!-- Rendera vyer här -->
+  </main>
+  <Footer /> <!-- Lägg till Footer-komponenten -->
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  min-height: 80vh;
+  width: 100%;
+  max-width: 1200px;
+  margin: 60px auto 2rem auto;
+  padding: 1rem;
+}
+</style>
