8a82af42fa7f4f801fc7da751d772d786c526ab2
Isa Westling <isawestling.blogg@hotmail.com>
2024-12-09 13:24:03 +0100

    Uppdaterat HomeView.vue med nytt innehåll och layout för att beskriva fritidsintressen.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,34 @@
 <template>
-  <main>
-    <h1>Hem</h1>
-    <p>Testar lite text...</p>
-  </main>
+  <!-- Container för innehållet -->
+  <div class="container">
+    <!-- Rubrik och text -->
+    <h2 class="mb-3">Mitt fritidsintresse</h2>
+    <p>Mitt största fritidsintresse är att resa och upptäcka nya platser. Tillsammans med min sambo och vår hund Lilo
+      spenderar vi så mycket
+      tid som möjligt på vägarna i vår plåtis. Det är något speciellt med friheten att kunna åka vart man vill och och
+      stanna när man vill,
+      samtidigt som man har med sig sitt hem.</p>
+    <p>En plats som ligger oss varmt om hjärtat är Norge, ett land vi har utforskat från norr till söder. Våra absoluta
+      favoritställen är
+      Möre och Romsdal samt Lofoten. Naturen där är helt magisk, och det finns alltid nya vandringar och platser att
+      upptäcka.</p>
+    <p>Utöver Norge tycker vi om att ge oss ut på roadtrips längre söderut i Europa. Vi har varit i 13 länder hittills,
+      och Italien är en
+      stor favorit, där vi brukar bo på olika vingårdar. Det är svårt att slå kombinationen av vacker natur, fantastisk
+      mat och härliga människor. Generellt
+      gillar vi platser som har mycket natur, god mat och nya äventyr att erbjuda!</p>
+  </div>
+  <!-- Bild  -->
+  <div class="text-center mt-4">
+    <img src="@/assets/vineyard.jpg" alt="Vingård" class="img-fluid" />
+  </div>
 </template>
 
 <script>
-
+// Exportera vy-komponenten
+export default {
+  name: 'HomeView'
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
